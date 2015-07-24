daa432c46027283194365b767e8528a6b6870d80
Robbie Cooper <cooperra@users.noreply.github.com>
2015-07-24 04:12:19 -0400

    Copy _factory attribute to fix error

--- TokenFilter.py
+++ TokenFilter.py
@@ -5,6 +5,8 @@ class TokenFilter(TokenSource):
     def __init__(self, source:TokenSource):
         super().__init__()
         self._source = source
+        # copy attributes
+        self._factory = source._factory
 
     def nextToken(self):
         """Override this method."""
