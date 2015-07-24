80e28cd54e9bc370c41d15e963be22c9f0b16941
Robbie Cooper <cooperra@users.noreply.github.com>
2015-07-24 17:48:05 -0400

    Remove unneeded methods in TokenFilter

--- TokenFilter.py
+++ TokenFilter.py
@@ -11,21 +11,3 @@ class TokenFilter(TokenSource):
     def nextToken(self):
         """Override this method."""
         return self._source.nextToken()
-
-    def getLine(self):
-        return self._source.nextToken()
-
-    def getCharPositionInLine():
-        return self._source.getCharPositionInLine()
-
-    def getInputStream():
-        return self._source.getInputStream()
-
-    def getSourceName():
-        return self._source.getSourceName()
-
-    def setTokenFactory():
-        return self._source.setTokenFactory()
-
-    def getTokenFactory():
-        return self._source.getTokenFactory()
